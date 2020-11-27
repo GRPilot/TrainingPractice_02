f5571f7cda0c5098ef507978c00b514cc75f56e9
GRPilot <rusan.rusik@yandex.ru>
2020-11-27 19:18:56 +0300

    clean up

--- Scripts/GameWindowScripts/GameWindow.cs
+++ Scripts/GameWindowScripts/GameWindow.cs
@@ -2,10 +2,6 @@ using Godot;
 using System;
 
 public class GameWindow : Node2D {
-	public override void _Ready() {
-
-	}
-
 	public override void _Process(float delta) {
 		if(Input.IsKeyPressed(Convert.ToInt32(KeyList.Escape))) {
 			GetTree().ChangeScene("res://Scenes/MainMenu.tscn");
--- Scripts/MainMenuScripts/ExitButton.cs
+++ Scripts/MainMenuScripts/ExitButton.cs
@@ -2,10 +2,6 @@ using Godot;
 using System;
 
 public class ExitButton : Button {
-	public override void _Ready() {
-		
-	}
-
 	private void OnExitButtonPressed() {
 		GetTree().Quit();
 	}
