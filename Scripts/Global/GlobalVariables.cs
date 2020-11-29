a0e70fed85fa634847d3432895d5690dfbc9ae7b
GRPilot <rusan.rusik@yandex.ru>
2020-11-29 16:37:19 +0300

    Add GameMode Global Variables

--- Scripts/Global/GlobalVariables.cs
+++ Scripts/Global/GlobalVariables.cs
@@ -9,8 +9,26 @@ public class GlobalVariables : Node {
         public const string LinuxPath = GameShortName + "/"; //"~/.local/share/" + GameShortName + "/";
     }
 
-    public struct GameMode {
+    public class GameMode {
+        public enum Mode {
+            Speed,
+            Time,
+        }
+        public static Mode currentMode = Mode.Speed;
         public const string Speed = "SpeedMode";
         public const string Time = "TimeMode";
+
+        public static readonly Vector2 SpeedDotsCount = new Vector2(10, 10);
+        public static readonly Vector2 TimeDotsCount = new Vector2(20, 20);
+
+        public static Vector2 GetDotsCount() {
+            switch(currentMode) {
+                case Mode.Speed:
+                    return SpeedDotsCount;
+                case Mode.Time:
+                    return TimeDotsCount;
+            }
+            return SpeedDotsCount;
+        }
     }
 }
