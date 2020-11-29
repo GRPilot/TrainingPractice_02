c4d10d5885b68c0adef84ae1c512354d335004f1
GRPilot <rusan.rusik@yandex.ru>
2020-11-29 13:45:36 +0300

    Change timer setting

--- Scripts/GameWindowScripts/GameWindow.cs
+++ Scripts/GameWindowScripts/GameWindow.cs
@@ -2,45 +2,44 @@ using Godot;
 using System;
 
 public class GameWindow : Node2D {
-	private PackedScene ResultPanelBuilder
-		= ResourceLoader.Load("res://Scripts/Prefubs/ResultPanel.cs") as PackedScene;
+    private PackedScene ResultPanelBuilder
+        = ResourceLoader.Load("res://Scripts/Prefubs/ResultPanel.cs") as PackedScene;
 
-	public override void _Ready() {
-		GetNode("DrawAreaContainer")
-			.Connect("StartGame", this, nameof(OnStartGame));
+    public override void _Ready() {
+        GetNode("DrawAreaContainer")
+            .Connect("StartGame", this, nameof(OnStartGame));
 
-		GetNode("DrawAreaContainer")
-			.Connect("AllCirclesActivated", this, nameof(OnAllCirclesActivated));
-	}
+        GetNode("DrawAreaContainer")
+            .Connect("AllCirclesActivated", this, nameof(OnAllCirclesActivated));
+    }
 
-	public override void _Process(float delta) {
-		if(Input.IsKeyPressed(Convert.ToInt32(KeyList.Escape))) {
-			GetTree().ChangeScene("res://Scenes/MainMenu.tscn");
-		}
-	}
+    public override void _Process(float delta) {
+        if(Input.IsKeyPressed(Convert.ToInt32(KeyList.Escape))) {
+            GetTree().ChangeScene("res://Scenes/MainMenu.tscn");
+        }
+    }
 
-	private void OnStartGame() {
-		Timer timer = GetNode<Timer>("Timer");
-		if(null == timer) {
-			GetNode<Label>("StatisticContainer/TimeLabel")
-				.Text = "Timer was broken by someone";
-		}
-		timer.WaitTime = .01f;
-		timer.Start();
-	}
-	private void OnAllCirclesActivated(int score) {
-		ShowResults("You are win!", score);
-	}
+    private void OnStartGame() {
+        Timer timer = GetNode<Timer>("Timer");
+        if(null == timer) {
+            GetNode<Label>("StatisticContainer/TimeLabel")
+                .Text = "Timer was broken by someone";
+        }
+        timer.Start();
+    }
+    private void OnAllCirclesActivated(int score) {
+        ShowResults("You are win!", score);
+    }
 
-	private void ShowResults(string title, int score) {
-		GD.Print(title);
-		ResultPanel resultPanel = GetNode<ResultPanel>("ResultPanel");
-		resultPanel.SetTitle(title);
-		resultPanel.SetScore(score.ToString());
-		resultPanel.SetTime(
-			GetNode<TimeLabel>("StatisticContainer/TimeLabel").GetTime()
-		);
-		resultPanel.PopupCentered();
-	}
+    private void ShowResults(string title, int score) {
+        GD.Print(title);
+        ResultPanel resultPanel = GetNode<ResultPanel>("ResultPanel");
+        resultPanel.SetTitle(title);
+        resultPanel.SetScore(score.ToString());
+        resultPanel.SetTime(
+            GetNode<TimeLabel>("StatisticContainer/TimeLabel").GetTime()
+        );
+        resultPanel.PopupCentered();
+    }
 
 }
