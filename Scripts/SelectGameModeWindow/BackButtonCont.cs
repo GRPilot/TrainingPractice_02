8ef64ed9ac565f87738905af16ff6eebd843f93f
GRPilot <rusan.rusik@yandex.ru>
2020-11-29 20:44:23 +0300

    remove useless method

--- Scripts/SelectGameModeWindow/BackButtonCont.cs
+++ Scripts/SelectGameModeWindow/BackButtonCont.cs
@@ -2,9 +2,6 @@ using Godot;
 using System;
 
 public class BackButtonCont : HBoxContainer {
-    public override void _Ready() {
-
-    }
 
     public void OnBackMenuButtonPressed() {
         GetTree().ChangeScene("res://Scenes/MainMenu.tscn");
