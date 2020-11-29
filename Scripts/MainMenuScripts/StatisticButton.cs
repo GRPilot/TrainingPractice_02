ecdd34a81a087f3654f33a0f995b5c11a8ba737a
GRPilot <rusan.rusik@yandex.ru>
2020-11-29 13:42:42 +0300

    Add Statistics scene

---
+++ Scripts/MainMenuScripts/StatisticButton.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class StatisticButton : Button {
+    static int uid = 0;
+    private void OnStatisticButtonPressed() {
+    }
+}
